b105872d056825a53bda996ef543092aa573bbf0
Stuti Srivastava <stutisrivastv@gmail.com>
2018-06-03 13:24:35 +0530

    @ssrivastava - Add repo root

--- src/test/java/com/learn/springcloudcontractclient/LoanApplicationServiceTests.java
+++ src/test/java/com/learn/springcloudcontractclient/LoanApplicationServiceTests.java
@@ -19,6 +19,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 @AutoConfigureStubRunner(ids = {"com.learn:spring-cloud-contract-server:+:stubs:6565"},
         stubsMode = StubRunnerProperties.StubsMode.LOCAL,
+        repositoryRoot = "http://repo.spring.io/libs-snapshot",
 mappingsOutputFolder = "target/outputmappings/")
 @DirtiesContext
 public class LoanApplicationServiceTests {
